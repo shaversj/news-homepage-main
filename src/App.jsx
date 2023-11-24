0b48453433e0d99e4704af010377ee288cee72a5
Josh Shavers <shaversj@gmail.com>
2023-11-24 10:26:53 -0600

    add main tag

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   return (
     <>
       <div className={"bg-black min-h-screen flex items-center"}>
-        <div className={"px-[15px] max-w-screen-sm bg-off-white lg:max-w-screen-lg lg:h-[1024px] lg:w-full mx-auto " + "font-Inter lg:px-[164px] lg:grid lg:grid-cols-[auto,auto,auto] lg:grid-rows-[auto,auto,auto,auto] lg:gap-x-[28px]"}>
+        <main className={"px-[15px] max-w-screen-sm bg-off-white lg:max-w-screen-lg lg:h-[1024px] lg:w-full mx-auto " + "font-Inter lg:px-[164px] lg:grid lg:grid-cols-[auto,auto,auto] lg:grid-rows-[auto,auto,auto,auto] lg:gap-x-[28px]"}>
           <Header />
           <Picture />
           <Section />
           <Aside />
           <Footer />
-        </div>
+        </main>
       </div>
     </>
   );
